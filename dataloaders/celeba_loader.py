20cf428b115eb3204091355ad1199405a0c068af
Luuk99 <luuk9926@gmail.com>
2021-01-08 15:01:06 +0100

    Dataloader batch size updated

--- dataloaders/celeba_loader.py
+++ dataloaders/celeba_loader.py
@@ -34,12 +34,12 @@ def load_data():
 
 	trainset = torchvision.datasets.CelebA(root='./data/celeba', train=True,
                                         download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=2)
 
 	testset = torchvision.datasets.CelebA(root='./data/celeba', train=False,
                                        download=True, transform=transform)
-	testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+	testloader = torch.utils.data.DataLoader(testset, batch_size=128,
                                          shuffle=False, num_workers=2)
 									
 	return trainloader, testloader
--- dataloaders/cifar100_loader.py
+++ dataloaders/cifar100_loader.py
@@ -34,12 +34,12 @@ def load_data():
 
 	trainset = torchvision.datasets.CIFAR100(root='./data/cifar100', train=True,
                                         download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=2)
 
 	testset = torchvision.datasets.CIFAR100(root='./data/cifar100', train=False,
                                        download=True, transform=transform)
-	testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+	testloader = torch.utils.data.DataLoader(testset, batch_size=128,
                                          shuffle=False, num_workers=2)
 
 	classes = ('plane', 'car', 'bird', 'cat',
