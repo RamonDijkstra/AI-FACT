2472f7f1672cd53ac6a26818c7933c308d570b18
Luuk99 <luuk9926@gmail.com>
2021-01-08 14:37:44 +0100

    Cifar10 dataloader

---
+++ dataloaders/cifar10_loader.py
@@ -0,0 +1,48 @@
+###############################################################################
+# MIT License
+#
+# Copyright (c) 2020
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Authors: Luuk Kaandorp, Ward Pennink, Ramon Dijkstra, Reinier Bekkenutte 
+# Date Created: 2020-01-08
+###############################################################################
+
+"""
+Helper function that loads CIFAR10 data
+"""
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def load_data():
+	'''
+    loads the CIFAR10 dataset and splits into
+	train and test sets
+    '''
+	
+	transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+	trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+	testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+	testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+	classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+	return trainloader, testloader
