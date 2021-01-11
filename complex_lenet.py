dea7a8734bd9e003be309646b4ee63407d0428a7
WardP123 <wardpennink@hotmail.nl>
2021-01-11 12:04:43 +0100

    Added complex functions

--- complex_lenet.py
+++ complex_lenet.py
@@ -1,39 +1,75 @@
+# Adapted from https://github.com/wavefrontshaping/complexPyTorch
+from complexPyTorch-master.complexFunctions import *
+from complexPyTorch-master.complexLayers import *
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
 
-class Generator(nn.Module):
 
-    def __init__(self, k):
+class Encoder(nn.Module):
+
+    def __init__(self, k, size, magnitude):
         """
         """
         super().__init__()
-        self.b = torch.rand()
-        
+
+        # k - 1 features will be generated
+        self.k = k
+
+        # b is normalized to have same magnitude as a
+        b = torch.randn(size)
+        divider = torch.linalg.norm(b) / magnitude
+        self.b = b / divider
+
+        # Tensor of angles that rotate real feature, sampled from uniform
+        # distribution between 0 and pi (excluding 0 itself)
         self.thetas = nn.Parameter(torch.empty(k-1))
-        self.thetas = toch.rand(k-1) * torch.pi
+        self.thetas = torch.cuda.FloatTensor(k-1).uniform_(0, np.pi)
 
 
     def forward(self, a):
         """
         """
 
-        x = self.layers(z)
-        x = x.view(z.shape[0], self.output_shape[0], self.output_shape[1], self.output_shape[2])
-        
+        # Do we also add the real feature (i.e. a) to x ..?
+        x = torch.empty(self.k)
+        x[0] = a
+        for i in range(1, self.k):
+            x[i] = (a + self.b *1j) * torch.exp(1j * self.thetas[i])
         return x
 
-    @property
-    def device(self):
-        """
-        """
-        return next(self.parameters()).device
+
+class Lenet_processing_module(nn.Module):
+    def __init__(self, k):
+        super(Lenet_processing_module, self).__init__()
+        self.conv2 = ComplexConv2d(6, 16, 3, bias=False)
+        # an affine operation: y = Wx + b
+        self.fc1 = ComplexLinear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc2 = ComplexLinear(120, 84)
+        self.fc3 = ComplexLinear(84, 10)
+    
+    def forward(self, x):
+        temp = self.conv2(x)
+        temp = self.fc1(temp)
+        temp = self.fc2(temp)
+        out = self.fc3(temp)
+        
+        return out
+
+class Lenet_decoder(nn.Module):
+    def __init__(self):
+        super(Lenet_decoder, self).__init__()
+
+    def forward(self, x):
+        pass
 
 
 class Discriminator(nn.Module):
 
-    def __init__(self, input_dims=784, hidden_dims=[512, 256], dp_rate=0.3):
+    def __init__():
         """
         """
         super().__init__()
@@ -57,7 +93,9 @@ class Lenet_encoder(nn.Module):
         # conv1 is the encoder which maps the input to the feature 
         a = self.conv1(x)
         # We use the feature to generate k - 1 fake features
-        k = self.generator(a)
+        size = a.shape
+        magnitude = torch.linalg.norm(a)
+        k = self.generator(self.k, size, magnitude)
         return x
 
     def num_flat_features(self, x):
@@ -66,23 +104,3 @@ class Lenet_encoder(nn.Module):
         for s in size:
             num_features *= s
         return num_features
-
-class Lenet_processing_module(nn.Module):
-    def __init__(self):
-        super(Lenet_processing_module, self).__init__()
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-    
-    def forward(self, x):
-        pass
-
-class Lenet_decoder(nn.Module):
-    def __init__(self):
-        super(Lenet_decoder, self).__init__()
-
-    def forward(self, x):
-        pass
-        
