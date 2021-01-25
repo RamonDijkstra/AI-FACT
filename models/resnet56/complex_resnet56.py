Fixed batch normalization

models/resnet56/complex_resnet56.py
@@ -151,6 +151,8 @@ class ComplexResNet56(pl.LightningModule):
         self.log("train_total-loss", loss)
         self.log("train_acc", acc)
 
+        return loss
+
     def validation_step(self, batch, optimizer_idx):
         """
         Validation step of the complex LeNet model.
@@ -193,6 +195,8 @@ class ComplexResNet56(pl.LightningModule):
         self.log("val_model_loss", model_loss)
         self.log("val_total-loss", loss)
         self.log("val_acc", acc)
+        
+        return loss
 
     def test_step(self, batch, batch_idx):
         """
@@ -458,7 +462,11 @@ class ResNetBlock(nn.Module):
             intermediate_real, intermediate_imag = complex_conv(encoded_batch_real, encoded_batch_imag, self.conv1_real, self.conv1_imag)
             intermediate_real, intermediate_imag = complex_batchnorm(intermediate_real), complex_batchnorm(intermediate_imag)
 
-            intermediate_real, intermediate_imag = complex_relu(intermediate_real, self.device, c=1), complex_relu(intermediate_imag, self.device, c=1)
+            intermediate_real, intermediate_imag = complex_relu(
+                intermediate_real, self.device, torch.complex(intermediate_real, intermediate_imag), c=1
+            ), complex_relu(
+                intermediate_imag, self.device, torch.complex(intermediate_real, intermediate_imag), c=1
+            )
 
             intermediate_real, intermediate_imag = complex_conv(intermediate_real, intermediate_imag, self.conv2_real, self.conv2_imag)
             intermediate_real, intermediate_imag = complex_batchnorm(intermediate_real), complex_batchnorm(intermediate_imag)
@@ -471,7 +479,11 @@ class ResNetBlock(nn.Module):
             intermediate_real = intermediate_real + encoded_batch_real
             intermediate_imag = intermediate_imag + encoded_batch_imag
 
-            intermediate_real, intermediate_imag = complex_relu(intermediate_real, self.device, c=1), complex_relu(intermediate_imag, self.device, c=1)
+            intermediate_real, intermediate_imag = complex_relu(
+                intermediate_real, self.device, torch.complex(intermediate_real, intermediate_imag), c=1
+            ), complex_relu(
+                intermediate_imag, self.device, torch.complex(intermediate_real, intermediate_imag), c=1
+            )
 
             # recombine the real and imaginary parts into a complex feature
             out = torch.complex(intermediate_real, intermediate_imag)

utils.py
@@ -47,7 +47,7 @@ def complex_conv(x_real, x_imag, conv_real, conv_imag):
     # return the convolved real and imaginary parts
     return real_out, imag_out
 
-def complex_relu(x, device, c=None):
+def complex_relu(x, device, x_complex=None, c=None):
     """
     Function to apply ReLu on complex features.
 
@@ -63,8 +63,11 @@ def complex_relu(x, device, c=None):
         result - Resulting features after ReLU. [B, C, W, H]
     """
 
+    # TODO is this correct..?
+    norm_x = x_complex if x_complex is not None else x
+    
     # calculate the denominator
-    norm = complex_norm(x)
+    norm = complex_norm(norm_x)
 
     # check whether to give the given value of c
     if c is not None:
@@ -134,7 +137,7 @@ def complex_max_pool(x, pool):
     # return the values with the highest norm
     return output
 
-def complex_batchnorm(complex_tensor):
+def complex_batchnorm(x, x_complex=None):
     """
     Function to apply batch normalization on complex features.
 
@@ -150,19 +153,15 @@ def complex_batchnorm(complex_tensor):
 
     ###BATCH size x Dim x Dim x Dim
 
-
-    denominator = complex_tensor.view(complex_tensor.shape[0],-1)
-    #   print(denominator.shape)
+    norm_x = x_complex if x_complex is not None else x
+    
+    denominator = norm_x.view(norm_x.shape[0],-1)
     denominator = complex_norm(denominator)**2
-    #  print(denominator.shape)
     denominator = denominator.mean(dim=1)
+    denominator = torch.sqrt(denominator)
     denominator = denominator.view(denominator.shape[0],1,1,1)
-    #  print(denominator.shape)
 
-
-    # print(denominator.shape)
-
-    result = complex_tensor/denominator
+    result = x/denominator
 
 
     #print(result.shape)