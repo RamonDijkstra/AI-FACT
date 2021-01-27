Fixed softmax input

models/encoder/GAN.py
@@ -221,14 +221,13 @@ class Generator(nn.Module):
         b = (b / b_magnitude) * a_magnitude
 
         # sample angles to rotate the features for the real rotation
-        # thetas = torch.Tensor(image_dimensions[0], 1, 1, 1).uniform_(0, 2 * np.pi).to(self.device)
-        thetas = torch.ones(image_dimensions[0], 1, 1, 1).to(self.device)
-        # thetas = torch.exp(1j * thetas)
-        
-        # compute encoded real feature
-        # x = (a + b * 1j) * thetas
-        # x = torch.complex(a,b) * torch.exp(1j * thetas)
-        x = torch.complex(a,b) * thetas
+        thetas = torch.Tensor(image_dimensions[0], 1, 1, 1).uniform_(0, 2 * np.pi).to(self.device)
+        #thetas = torch.ones(image_dimensions[0], 1, 1, 1).to(self.device)
+        #thetas = torch.exp(1j * thetas)
+        x =  torch.complex(a,b) * torch.exp(1j * thetas)
+
+        #x = torch.complex(a,b) * thetas
+
         x = x.to(self.device)
         
         # check if training
@@ -241,12 +240,10 @@ class Generator(nn.Module):
         
             # sample k-1 delta angles to rotate the features for fake examples
             delta_thetas = torch.Tensor((self.k-1) * image_dimensions[0], 1, 1, 1).uniform_(0, np.pi).to(self.device)
-            # delta_thetas = torch.exp(1j * delta_thetas)
         
             # compute encoded fake features
             fake_a = torch.cat([a]*(self.k-1),dim=0)
             fake_x = torch.complex(fake_a, fake_b) * torch.exp(1j * delta_thetas)
-            # fake_x = (fake_a + fake_b *1j) * delta_thetas
             fake_x = fake_x.to(self.device)
             
             # return real feature, real encoded feature, thetas, fake encoded feature and delta thetas

models/lenet/complex_lenet.py
@@ -108,15 +108,15 @@ class ComplexLeNet(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from the processing module
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
 		
 		# calculate the predictions
-        result = self.softmax(result)
+        result = self.softmax(out)
         preds = result.argmax(dim=-1)
         acc = (labels == preds).float().mean()
         
         # calculate the model loss 
-        model_loss = self.loss_fn(result, labels)
+        model_loss = self.loss_fn(out, labels)
         loss = gan_loss + model_loss
 
         # log the training loss and accuracy
@@ -156,15 +156,15 @@ class ComplexLeNet(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from the processing module
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
         
         # calculate the predictions
-        result = self.softmax(result)
+        result = self.softmax(out)
         preds = result.argmax(dim=-1)
         acc = (labels == preds).float().mean()
 
 		# calculate the loss
-        model_loss = self.loss_fn(result, labels)
+        model_loss = self.loss_fn(out, labels)
         loss = gan_loss + model_loss
         
         # log the validation loss and accuracy
@@ -204,15 +204,15 @@ class ComplexLeNet(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from the processing unit
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
         
         # calculate the predictions
-        result = self.softmax(result)
+        result = self.softmax(out)
         preds = result.argmax(dim=-1)
         acc = (labels == preds).float().mean()
         
         # calculate the loss
-        model_loss = self.loss_fn(result, labels)
+        model_loss = self.loss_fn(out, labels)
         loss = gan_loss + model_loss
         
         # log the test loss and accuracy
@@ -271,7 +271,6 @@ class LenetProcessingModule(nn.Module):
         intermediate_real, intermediate_imag = complex_relu(encoded_batch_real, encoded_batch_imag, self.device)
         intermediate_real, intermediate_imag = complex_max_pool(intermediate_real, intermediate_imag, self.pool)
 
-        # intermediate_real, intermediate_imag = complex_conv(encoded_batch_real, encoded_batch_imag, self.conv2_real, self.conv2_imag)
         intermediate_real, intermediate_imag = complex_conv(intermediate_real, intermediate_imag, self.conv2_real, self.conv2_imag)
         intermediate_real, intermediate_imag = complex_relu(intermediate_real, intermediate_imag, self.device)        
         intermediate_real, intermediate_imag = complex_max_pool(intermediate_real, intermediate_imag, self.pool)
@@ -317,9 +316,6 @@ class LenetDecoder(nn.Module):
         # save the inputs
         self.num_classes = num_classes
 
-        # initialize the softmax layer
-        self.softmax = nn.Softmax(dim=1)
-
 
     def forward(self, encoded_batch, thetas):
         """
@@ -341,11 +337,11 @@ class LenetDecoder(nn.Module):
         """
 
     	# rotate the features back to their original state
-        decoded_batch = encoded_batch * torch.exp(-1j * thetas)
+        decoded_batch = encoded_batch * torch.exp(-1j * thetas.squeeze())[:, None]
         
         # get rid of the imaginary part of the complex features
         # decoded_batch = decoded_batch.real
-        decoded_batch = encoded_batch.real
+        decoded_batch = decoded_batch.real
         
         # return the decoded batch
         return decoded_batch

models/vgg16/vgg16_complex.py
@@ -160,17 +160,17 @@ class Complex_VGG16(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
 
         # log the train accuracy
-        out = self.softmax(result)
+        result = self.softmax(out)
         preds = out.argmax(dim=-1)
         acc = (labels == preds).float().mean()
         self.log('train_acc', acc)
         
         # return the decoded feature, discriminator predictions and real labels
         # return x, discriminator_predictions, labels
-        model_loss = self.loss_fn(result, labels)
+        model_loss = self.loss_fn(out, labels)
         loss = gan_loss + model_loss
 
         # log the loss
@@ -213,14 +213,14 @@ class Complex_VGG16(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from the processing unit
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
         
         # calculate the predictions
-        result = self.softmax(result)
+        result = self.softmax(out)
         preds = result.argmax(dim=-1)
         acc = (labels == preds).float().mean()
 
-        model_loss = self.loss_fn(result, labels)
+        model_loss = self.loss_fn(out, labels)
         loss = gan_loss + model_loss
         
         # log the validation accuracy
@@ -260,14 +260,17 @@ class Complex_VGG16(pl.LightningModule):
         out = self.proccessing_module(out)
         
         # decode the feature from the processing unit
-        result = self.decoder(out, thetas)
+        out = self.decoder(out, thetas)
         
         # calculate the predictions
-        result = self.softmax(result)
+        result = self.softmax(out)
         preds = result.argmax(dim=-1)
         acc = (labels == preds).float().mean()
-        
-        # log the test accuracy
+
+        loss = self.loss_fn(out, labels)
+
+      # log the test loss and accuracy
+        self.log("test_loss", loss)
         self.log('test_acc', acc)
 
 class VGG16ProcessingModule(nn.Module):