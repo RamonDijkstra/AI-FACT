e651baa34ade2f04e78273883138a91af1cccc8d
Luuk Kaandorp <luuk9926@gmail.com>
2021-01-28 16:38:55 +0100

    Dataloader comments

--- dataloaders/cifar10_loader.py
+++ dataloaders/cifar10_loader.py
@@ -10,46 +10,57 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to conditions.
 #
-# Authors: Luuk Kaandorp, Ward Pennink, Ramon Dijkstra, Reinier Bekkenutte 
+# Authors: Luuk Kaandorp, Ward Pennink, Ramon Dijkstra, Reinier Bekkenutte
 # Date Created: 2020-01-08
 ###############################################################################
 
 """
-Helper function that loads CIFAR10 data
+Helper function that loads CIFAR-10 data
 """
 
+# pytorch imports
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def load_data(batch_size=128, num_workers=2):
+def load_data(batch_size=128, num_workers=0):
     '''
-    loads the CIFAR10 dataset and splits into
-    train and test sets
+    Function loads the CIFAR-10 dataset and splits into
+    train, validation and test sets.
+
+    Inputs:
+        batch_size - Int indicating the batch size. Default = 256
+        num_workers - Int indicating the number of workers for loading
+            the data. Default = 0 (truly deterministic)
     '''
 
+    # normalize the input
     transform = transforms.Compose(
     [transforms.ToTensor(),
        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
        ])
 
+    # load the training and test dataset
     trainset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=True,
                                         download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=num_workers)
-
     testset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=False,
                                         download=True, transform=transform)
 
+    # split the test dataset into test and validation
     val_split = int(len(testset)/2)
     test_split = len(testset) - val_split
     valset, testset = torch.utils.data.random_split(testset, [val_split, test_split])
 
+    # create the dataloaders
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
     valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
                                             shuffle=False, num_workers=num_workers)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                                 shuffle=False, num_workers=num_workers)
 
+    # 10 classes for CIFAR-10
     num_classes = 10
 
+    # return the classes and dataloaders
     return num_classes, trainloader, valloader, testloader
--- dataloaders/cub2011_loader.py
+++ dataloaders/cub2011_loader.py
@@ -1,5 +1,30 @@
+###############################################################################
+# MIT License
+#
+# Copyright (c) 2020
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Authors: Luuk Kaandorp, Ward Pennink, Ramon Dijkstra, Reinier Bekkenutte
+# Date Created: 2020-01-08
+###############################################################################
+
+"""
+Helper function that loads CUB-200 2011 data
+"""
+
+# basic imports
 import os
 import pandas as pd
+import tarfile
+from os import path
+
+# pytorch imports
 import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
@@ -8,35 +33,51 @@ from torchvision.datasets.utils import download_url
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-import tarfile
-from os import path
+def load_data(batch_size=128, num_workers=0):
+    '''
+    Function loads the CUB-200 dataset and splits into
+    train, validation and test sets.
 
-def load_data(batch_size=128, num_workers=2):
+    Inputs:
+        batch_size - Int indicating the batch size. Default = 256
+        num_workers - Int indicating the number of workers for loading
+            the data. Default = 0 (truly deterministic)
+    '''
+
+    # normalize the input
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         transforms.Resize(size=(56,56))]
     )
 
+    # create path with images
     if not path.exists('./data/CUB_200_2011/CUB_200_2011/images'):
+        # check if tar file has been downloaded
+        if not path.exists('./data/CUB_200_2011/CUB_200_2011.tgz'):
+            # alert user of missing tar file
+            assert False, "CUB-200 tar file missing. Place inside './data/CUB200_2011' folder. Please download via https://drive.google.com/file/d/1hbzc_P1FuxMkcabkgn9ZKinBwW683j45/view"
+        print('Unpacking tar file..')
         with tarfile.open(os.path.join('./data/CUB_200_2011', 'CUB_200_2011.tgz'), "r:gz") as tar:
             tar.extractall(path='./data/CUB_200_2011')
+        print('Unpacking finished')
 
-    # Train data
+    # retrieve the dataset
     data_set = ImageFolder('./data/CUB_200_2011/CUB_200_2011/images', transform=transform)
 
+    # split the data in 70% train, 15% test and 15% validation
     train_split = int(0.7 * len(data_set))
     val_test_split = len(data_set) - train_split
     trainset, val_testset = torch.utils.data.random_split(
         data_set, [train_split, val_test_split]
     )
-
     val_split = int(len(val_testset)/2)
     test_split = len(val_testset) - val_split
     valset, testset = torch.utils.data.random_split(
         val_testset, [val_split, test_split]
     )
 
+    # create the dataloaders
     trainloader = DataLoader(
         trainset, batch_size=batch_size, num_workers=num_workers, shuffle=True
     )
@@ -47,80 +88,8 @@ def load_data(batch_size=128, num_workers=2):
         testset, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
 
+    # 200 classes for CUB-200
     num_classes = 200
 
+    # return the classes and dataloaders
     return num_classes, trainloader, valloader, testloader
-
-class Cub2011(Dataset):
-    base_folder = 'CUB_200_2011/images'
-    url = './data/CUB_200_2011.tgz'
-    filename = 'CUB_200_2011.tgz'
-    tgz_md5 = '97eceeb196236b17998738112f37df78'
-
-    def __init__(self, root=None, train=True, transform=None, loader=default_loader, download=True):
-        self.root = './data/CUB_200_2011'
-        self.transform = transform
-        self.loader = default_loader
-        self.train = train
-
-        if download:
-            self._download()
-
-        if not self._check_integrity():
-            raise RuntimeError('Dataset not found or corrupted.' +
-                               ' You can use download=True to download it')
-
-    def _load_metadata(self):
-        images = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'images.txt'), sep=' ',
-                             names=['img_id', 'filepath'])
-        image_class_labels = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'image_class_labels.txt'),
-                                         sep=' ', names=['img_id', 'target'])
-        train_test_split = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'train_test_split.txt'),
-                                       sep=' ', names=['img_id', 'is_training_img'])
-
-        data = images.merge(image_class_labels, on='img_id')
-        self.data = data.merge(train_test_split, on='img_id')
-
-        if self.train:
-            self.data = self.data[self.data.is_training_img == 1]
-        else:
-            self.data = self.data[self.data.is_training_img == 0]
-
-    def _check_integrity(self):
-        try:
-            self._load_metadata()
-        except Exception:
-            return False
-
-        for index, row in self.data.iterrows():
-            filepath = os.path.join(self.root, self.base_folder, row.filepath)
-            if not os.path.isfile(filepath):
-                print(filepath)
-                return False
-        return True
-
-    def _download(self):
-        import tarfile
-
-        if self._check_integrity():
-            print('Files already downloaded and verified')
-            return
-
-        # download_url(self.url, self.root, self.filename, self.tgz_md5)
-
-        with tarfile.open(os.path.join(self.root, self.filename), "r:gz") as tar:
-            tar.extractall(path=self.root)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        sample = self.data.iloc[idx]
-        path = os.path.join(self.root, self.base_folder, sample.filepath)
-        target = sample.target - 1  # Targets start at 1 by default, so shift to 0
-        img = self.loader(path)
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img, target
